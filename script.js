ffad019de685ceccb151f720d45d64323f4a3779
Ritish0744 <ritishverms@gmail.com>
2025-09-17 00:11:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ qr=document.getElementById("qr")
+        scn=document.getElementById("scn")
+        link=document.getElementById("link")
+
+
+
+        scn.onclick=function(){
+            window.location.href = "./scanner.html";
+        }
+        qr.onclick=function(){
+            window.location.href = "./qr.html";
+        }
+        
